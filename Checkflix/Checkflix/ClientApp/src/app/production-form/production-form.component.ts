768c5074d6c8f442cd7d95884105c420159a6c0f
wolframowsky <wolframowsky@gmail.com>
2020-04-14 15:20:31 +0200

    Implemented api call to secound service for more informations and improved card style

--- Checkflix/Checkflix/ClientApp/src/app/production-form/production-form.component.ts
+++ Checkflix/Checkflix/ClientApp/src/app/production-form/production-form.component.ts
@@ -44,10 +44,8 @@ export class ProductionFormComponent implements OnInit {
       ]]
 
     });
-    this.productionForm.valueChanges.subscribe(console.log)
+    //this.productionForm.valueChanges.subscribe(console.log)
 
-    // services
-    this.productionService.getProductions().subscribe(productionList => this.productionList = productionList);
     
   }
   get title() {
@@ -74,7 +72,7 @@ export class ProductionFormComponent implements OnInit {
 
   submitForm() {
     this.production = this.productionForm.value;
-    this.production.imbdRating = parseInt(this.productionForm.value.imbdRating);
+    this.production.imbdRating = parseFloat(this.productionForm.value.imbdRating);
     this.production.type = parseInt(this.productionForm.value.type);
 
 
@@ -85,28 +83,58 @@ export class ProductionFormComponent implements OnInit {
   fetchImbd() {
     const imbdId = this.productionForm.value.imbdId;
     if (imbdId) {
-      const url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + imbdId;
-      fetch(url, {
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-          "x-rapidapi-key": "8a5735bcd6msh35b94dd1467c587p1baf48jsn33eb07d88120"
-        }
-      })
-        .then(response => {
-          return response.json()
+
+      const movieDburl = `https://api.themoviedb.org/3/find/${imbdId}?api_key=61a4454e6812a635ebe4b24f2af2c479&language=pl_PL&external_source=imdb_id`;
+      fetch(movieDburl)
+        .then((response) => {
+          return response.json();
         })
         .then((data) => {
           console.log(data);
-          this.productionForm.controls.title.setValue(data.title);
-          this.productionForm.controls.releaseDate.setValue(data.year);
-          this.productionForm.controls.imbdRating.setValue(data.rating);
-          this.productionForm.controls.poster.setValue(data.poster);
-          this.productionForm.controls.synopsis.setValue(data.plot);
+
+          if (data.movie_results.length > 0) {
+            const movieArray = data.movie_results[0];
+
+            this.productionForm.controls.title.setValue(movieArray.title);
+            this.productionForm.controls.synopsis.setValue(movieArray.overview);
+            this.productionForm.controls.type.setValue(0);
+            this.productionForm.controls.releaseDate.setValue(new Date(movieArray.release_date));
+          }
+          else if (data.tv_results.length > 0) {
+            const seriesArray = data.tv_results[0];
+            this.productionForm.controls.title.setValue(seriesArray.name);
+            this.productionForm.controls.synopsis.setValue(seriesArray.overview);
+            this.productionForm.controls.type.setValue(1);
+            this.productionForm.controls.releaseDate.setValue(new Date(seriesArray.first_air_date));
+          }
+
+
+          const rapidApiUrl = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + imbdId;
+          fetch(rapidApiUrl, {
+            "method": "GET",
+            "headers": {
+              "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+              "x-rapidapi-key": "8a5735bcd6msh35b94dd1467c587p1baf48jsn33eb07d88120"
+            }
+          })
+            .then(response => {
+              return response.json()
+            })
+            .then((data) => {
+              console.log(data);
+              this.productionForm.controls.imbdRating.setValue(data.rating);
+              this.productionForm.controls.poster.setValue(data.poster);
+
+            })
+            .catch(err => {
+              console.log(err);
+            });
+
         })
         .catch(err => {
           console.log(err);
         });
+
     }
     else {
       this.imbdFetchClicked = true;
