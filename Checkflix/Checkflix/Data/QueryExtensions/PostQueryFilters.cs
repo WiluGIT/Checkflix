b5d79d54d51f84f69a4d6af945ee71a691aa2e40
Konrad Wilczek <wolframowsky@gmail.com>
2020-08-08 01:07:30 +0200

    Added all filters at  backend

--- Checkflix/Checkflix/Data/QueryExtensions/PostQueryFilters.cs
+++ Checkflix/Checkflix/Data/QueryExtensions/PostQueryFilters.cs
@@ -6,5 +6,12 @@ namespace Checkflix.Data.QueryExtensions
 
         public int PageNumber { get; set; }
         public string SearchQuery { get; set; }
+        public bool IsNetflix { get; set; } = true;
+        public bool IsHbo { get; set; } = true;
+        public int? YearFrom { get; set; }
+        public int? YearTo { get; set; }
+        public int? RatingFrom { get; set; }
+        public int? RatingTo { get; set; }
+
     }
 }
