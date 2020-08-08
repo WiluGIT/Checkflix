908f4df00717c22084dbbef88d2ebc75f73da8e7
Konrad Wilczek <wolframowsky@gmail.com>
2020-08-08 02:11:20 +0200

    Connected frontend filters to endpoints

--- Checkflix/Checkflix/ClientApp/src/app/home/home.component.ts
+++ Checkflix/Checkflix/ClientApp/src/app/home/home.component.ts
@@ -18,7 +18,13 @@ export class HomeComponent implements OnInit {
   postQueryFilters: IPostQueryFilters = {
     pageNumber: 1,
     pageSize: 25,
-    searchQuery: null
+    searchQuery: null,
+    isNetflix: null,
+    isHbo: null,
+    yearFrom: null,
+    yearTo: null,
+    ratingFrom: null,
+    ratingTo: null
   };
   activePageDataChunk: Array<IProductionViewModel> = [];
   // Filter form
@@ -52,23 +58,32 @@ export class HomeComponent implements OnInit {
       });
 
     this.productionsFilterForm = this.fb.group({
-      searchQuery: ''
+      searchQuery: '',
+      isNetflix: null,
+      isHbo: null,
+      yearFrom: null,
+      yearTo: null,
+      ratingFrom: null,
+      ratingTo: null
     });
 
     //Set variables
     this.maxValueYear = new Date().getFullYear();
     this.minValueYear = 1900;
-    this.minImbdRating = 0.0;
-    this.maxImbdRating = 10.0;
+    this.minImbdRating = 0;
+    this.maxImbdRating = 10;
   }
 
   getYearSliderValues(valueEmitted){
-    console.log("Emitted y: ", valueEmitted);
+    this.productionsFilterForm.controls["yearFrom"].setValue(parseInt(valueEmitted['left']));
+    this.productionsFilterForm.controls["yearTo"].setValue(parseInt(valueEmitted['right']));
   }
-  getImbdSliderValues(valueEmitted){
-    console.log("Emitted r: ", valueEmitted);
 
+  getImbdSliderValues(valueEmitted){
+    this.productionsFilterForm.controls["ratingFrom"].setValue(parseInt(valueEmitted['left']));
+    this.productionsFilterForm.controls["ratingTo"].setValue(parseInt(valueEmitted['right']));
   }
+
   checkBtn(e){
     const target = e.currentTarget;
     if(target.id ==="hbo-btn"){
@@ -76,22 +91,27 @@ export class HomeComponent implements OnInit {
         target.classList.add("vod-gray");
         target.classList.remove("hbo-btn");
         this.hboClicked = false;
+        this.productionsFilterForm.controls["isHbo"].setValue(false);
       }else{
         target.classList.remove("vod-gray");
         target.classList.add("hbo-btn");
         this.hboClicked = true;
+        this.productionsFilterForm.controls["isHbo"].setValue(null);
       }
     }else if(target.id === "netflix-btn"){
       if(this.netflixClicked){
         target.classList.add("vod-gray");
         target.classList.remove("netflix-btn");
         this.netflixClicked = false;
+        this.productionsFilterForm.controls["isNetflix"].setValue(false);
       }else{
         target.classList.remove("vod-gray");
         target.classList.add("netflix-btn");
         this.netflixClicked = true;
+        this.productionsFilterForm.controls["isNetflix"].setValue(null);
       }
     }
+    // At least one element must be clicked
     if(!this.hboClicked && !this.netflixClicked){
       this.hboClicked = true;
       this.netflixClicked = true;
@@ -99,6 +119,8 @@ export class HomeComponent implements OnInit {
       document.getElementById('netflix-btn').classList.remove("vod-gray");
       document.getElementById('hbo-btn').classList.add("hbo-btn");
       document.getElementById('netflix-btn').classList.add("netflix-btn");
+      this.productionsFilterForm.controls["isNetflix"].setValue(null);
+      this.productionsFilterForm.controls["isHbo"].setValue(null);
     }
   }
 
@@ -119,10 +141,22 @@ export class HomeComponent implements OnInit {
 
   applyFilters() {
     // Apply additional filters to query params
-    if (this.productionsFilterForm.controls["searchQuery"].value) {
+    if(this.productionsFilterForm.controls["searchQuery"].value) {
       this.postQueryFilters["searchQuery"] = this.productionsFilterForm.controls["searchQuery"].value;
     }
-
+    if(this.productionsFilterForm.controls["yearFrom"].value > 1900 || this.productionsFilterForm.controls["yearTo"].value < new Date().getFullYear()){
+      this.postQueryFilters["yearFrom"] = this.productionsFilterForm.controls["yearFrom"].value;
+      this.postQueryFilters["yearTo"] = this.productionsFilterForm.controls["yearTo"].value;
+    }
+    if(this.productionsFilterForm.controls["ratingFrom"].value > 0 || this.productionsFilterForm.controls["ratingTo"].value < 10){
+      this.postQueryFilters["ratingFrom"] = this.productionsFilterForm.controls["ratingFrom"].value;
+      this.postQueryFilters["ratingTo"] = this.productionsFilterForm.controls["ratingTo"].value;
+    }
+    if(this.productionsFilterForm.controls["isHbo"].value == false || this.productionsFilterForm.controls["isNetflix"].value == false){
+      this.postQueryFilters["isHbo"] = this.productionsFilterForm.controls["isHbo"].value;
+      this.postQueryFilters["isNetflix"] = this.productionsFilterForm.controls["isNetflix"].value;
+    }
+    console.log(this.postQueryFilters)
     // Change params to default
     this.postQueryFilters.pageNumber = 1;
     this.postQueryFilters.pageSize = 25;
@@ -139,6 +173,17 @@ export class HomeComponent implements OnInit {
       });
 
     // Reset fields
-    this.postQueryFilters.searchQuery = null;
+    //this.postQueryFilters.searchQuery = null;
+    this.postQueryFilters = {
+      searchQuery: '',
+      isNetflix: null,
+      isHbo: null,
+      yearFrom: null,
+      yearTo: null,
+      ratingFrom: null,
+      ratingTo: null,
+      pageNumber: 1,
+      pageSize: 25
+    };
   }
 }
